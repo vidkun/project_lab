318086ab32ba82e56be8590f3fc4807297ff343d
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-09 16:28:31 -0600

    generate projects controller with index, show, new, create, edit, update, and destroy actions and their views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'projects/index'
+
+  get 'projects/show'
+
+  get 'projects/new'
+
+  get 'projects/create'
+
+  get 'projects/edit'
+
+  get 'projects/update'
+
+  get 'projects/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
