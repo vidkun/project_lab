f2f8d3b002b8acf540099ea0e8ff3ea4e50ab602
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-09 23:13:09 -0600

    Write tests for tasks controller   * write tests of basic CRUD actions for tasks controller   * tests for get index and create task are currently failing

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,24 +1,37 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
+  setup do
+    @my_task = tasks(:one)
+  end
+
   test "should get index" do
-    get :index
+    get :index, project_id: @my_task.project_id
     assert_response :success
+    assert_not_nil assigns(:tasks)
   end
 
   test "should get show" do
-    get :show
+    get :show, { project_id: @my_task.project_id, id: @my_task }
     assert_response :success
   end
 
   test "should get new" do
-    get :new
+    get :new, project_id: @my_task.project_id
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, { project_id: @my_task.project_id, id: @my_task }
     assert_response :success
+  end
+
+  test "should create a task" do
+    assert_difference('Task.count') do
+      post :create, { project_id: @my_task.project_id, task: @my_task }
+    end
+
+    assert_redirected_to project_task_path(assigns(:task))
   end
 
 end
