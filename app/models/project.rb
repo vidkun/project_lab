f6c154c25b69c37bf2d6a350b92f52451be0c065
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-11 15:11:42 -0600

    Add validations for project model   * add validations to project model to ensure:       presence of all fields,       uniqueness of name,       minimum length of description,       Due Date must be in future

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,10 @@
 class Project < ActiveRecord::Base
   has_many :tasks
-  validates :name, presence: true, uniqueness: true
+  validates :name, :description, :due_date_at, presence: true
+  validates :name, uniqueness: true
+  validates :description, length: { minimum: 50 }
+  # validates :due_date_at, date: { after: Time.now } 
+  validates_each :due_date_at do |record, attr, value|
+    record.errors.add(attr, 'must be in the future') if !value.future?
+  end
 end
