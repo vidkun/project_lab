a9aa42d73e0089424f6a38df2b7fb7cf4bae5250
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-11 15:31:17 -0600

    add minimum length validation to task description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   validates :name, :description, presence: true
+  validates :description, length: { minimum: 50 }
   validates :delivery_minutes, numericality: { only_integer: true,
                                                greater_than: 0 }
 end
