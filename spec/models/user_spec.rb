8fe076fa54d24e9e80b6725720940b414650618e
Carlos Garcia <carlos@par9golf.com>
2014-09-22 02:17:51 -0600

    RSPEC: Added new spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when a new user is about to created" do
+    it 'should have name' do
+      user = User.new()
+      expect(user.valid?).to eq(false)
+      expect(user.errors[:name]).to eq(["can't be blank"])
+    end
+  end
 end
