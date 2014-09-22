8b35950b45dd907b2aca83dc9a8102b3b8650de3
Paul Cox <paulcox24@gmail.com>
2014-09-22 13:41:28 -0600

    Task rspec for name field

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Task, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when a new task is created" do
+    context "and it has invalid fileds" do
+      subject { build(:task)}
+
+      it 'check for name' do
+        expect(subject.valid?).to eq(false)
+        expect(subject.errors[:name]).to include("can't be blank")
+      end
+    end
+  end
 end
