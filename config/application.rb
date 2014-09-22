afc0a55b333fe234e82865ea9daab71a40c84248
Paul Cox <paulcox24@gmail.com>
2014-09-22 13:36:31 -0600

    Added rspec model task and factory task

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module ProjectLab
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     config.time_zone = "Mountain Time (US & Canada)"
+    config.generators do |g|
+      g.factory_girl dir: './spec/factories'
+    end
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
