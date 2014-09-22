77c487e4906d5d6c341ddb4ea4831e0a4af1b141
Carlos Garcia <carlos@par9golf.com>
2014-09-22 03:04:52 -0600

    Used factory girl in spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   context "when a new user is about to created" do
     it 'should have name' do
-      user = User.new()
+      user = build(:user)
       expect(user.valid?).to eq(false)
       expect(user.errors[:name]).to eq(["can't be blank"])
     end
@@ -13,6 +13,19 @@ RSpec.describe User, :type => :model do
       expect(user.valid?).to eq(false)
       expect(user.errors[:phone]).to include("is not a number")
       expect(user.errors[:phone]).to include("is the wrong length (should be 10 characters)")
+    end
+
+    it 'should have a valid email' do
+      user = build(:user)
+      expect(user.valid?).to eq(false)
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+
+    it 'will save with all attributes' do
+      user = build(:first_user)
+      Rails.logger.error user.errors
+      expect(user.valid?).to eq(true)
     end
   end
 end
