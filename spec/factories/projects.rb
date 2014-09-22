e25a04aa26b732d4f8ac41d673dd49f045b49062
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-22 13:45:10 -0600

    fix date format in projects factory

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,13 +3,13 @@ FactoryGirl.define do
     factory :first_project do
       name 'First Project Name'
       description 'MyText7890 MyText7890 MyText7890 MyText7890 MyText7890'
-      due_date_at 2015-09-09 15:17:16
+      due_date_at '2015-09-09 15:17:16'
     end
 
     factory :second_project do
       name 'Second Project Name'
       description 'MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo'
-      due_date_at 2015-09-09 15:17:38
+      due_date_at '2215-09-09 15:17:38'
     end
   end
 end
