e2cbc309492c9a940bca6e48bc6a3e00717ca440
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-23 11:59:06 -0600

    add expectations for GET index/show/new projects

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,18 +3,60 @@ require 'rails_helper'
 RSpec.describe ProjectsController, :type => :controller do
 
   login_user(FactoryGirl.create(:login_user))
-  subject { create(:second_project) }
+  let(:project) { create(:second_project) }
 
   describe "GET index" do
     it "successfully gets the index page" do
       get :index
       expect(response).to be_success
+      expect(response).to have_http_status(200)
       expect(response).to render_template("index")
     end
 
     it "assigns the @projects variable" do
       get :index
-      expect(assigns(:projects)).to eq(Project.all)
+      expect(assigns(:projects)).to eq([project])
     end
   end
+
+  describe "GET project" do
+    it 'successfully shows a project' do
+      get :show, id: project
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe "GET new" do
+    it "should get new" do
+      get :new
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  # describe "POST create" do
+  #   it "should create a project" do
+  #     post :create, project: FactoryGirl.attributes_for(:third_project)
+  #     expect(response).to be_success
+  #     expect(assigns(:project)).to be_a_new(Project)
+
+  #   end
+
+  #   # it "redirects to the project page upon save" do
+  #   #   post :create, project: FactoryGirl.attributes_for(:third_project)
+  #   #   expect(response).to redirect_to(project)
+  #   # end
+  # end
+
+# test "should create a project" do
+#     assert_difference('Project.count') do
+#       post :create, project: @update
+#     end
+
+#     assert_redirected_to project_path(assigns(:project))  
+#   end
+
+
 end
