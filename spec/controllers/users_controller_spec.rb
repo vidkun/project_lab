ad06f7856f70f517068386fd4d4d19684ad32891
Carlos Garcia <carlos@par9golf.com>
2014-09-23 10:43:40 -0600

    Added spec controller for user

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, :type => :controller do
+
+  describe "GET index" do
+    it "successfully gets the index page" do
+      get :index
+      expect(response).to be_success
+      expect(response).to render_template("index")
+    end
+
+    it "assigns the @users variable" do
+      get :index
+      expect(assigns(:users)).to eq(User.all)
+    end
+  end
+
+end
