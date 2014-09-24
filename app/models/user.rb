de2646a932114a5dba22f3ba1e71afe9c53e97bf
Carlos Garcia <carlos@par9golf.com>
2014-09-23 22:46:36 -0600

    TDD: Implement deleting a task via user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User < ActiveRecord::Base
   validate :name_is_not_test
   validates :phone, numericality: { greater_than: 0 }, length: { is: 10 }
 
+  def delete_task(task)
+    task_to_delete = self.tasks.find_by(id: task.id)
+    task_to_delete.destroy if task_to_delete
+  end
+
   private
   def name_is_not_test
     errors.add(:name, 'cannot be test') if self.name == 'test'
