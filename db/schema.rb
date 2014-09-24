a719f90381cd7df1e183d62555864f9ca662da6e
Carlos Garcia <carlos@par9golf.com>
2014-09-24 13:42:15 -0600

    Update task schema to accept creator as user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924070039) do
+ActiveRecord::Schema.define(version: 20140924180418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20140924070039) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "creator"
+    t.integer  "creator_id"
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
