624190260579a5d13d0a311feb4c074de67bbf23
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-23 20:05:45 -0600

    add update action expectation to registrations controller

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -4,13 +4,16 @@ RSpec.describe RegistrationsController, :type => :controller do
 
   before(:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
+    auth_user = FactoryGirl.create(:login_user)
+    allow(request.env['warden']).to receive(:authenticate!).and_return(auth_user)
+    allow(controller).to receive(:current_user).and_return(auth_user)
   end
 
   describe 'POST create' do
     context 'with valid info' do
       it 'should create a user' do
         expect {
-          post :create, user: FactoryGirl.attributes_for(:first_user)
+          post :create, user: FactoryGirl.attributes_for(:login_user)
           expect(response).to redirect_to(root_path)
         }.to change(User, :count).by(1)
       end
@@ -25,32 +28,16 @@ RSpec.describe RegistrationsController, :type => :controller do
     end
   end
 
-  # describe 'PUT update' do
-  #   # before :each do
-  #   #   @user = FactoryGirl.create(:login_user)
-  #   # end
-    
+  describe 'PUT update' do
+    context 'with valid attributes' do
+      it 'located the requested @user', :authorized => true do
+        user = User.last
 
-  #   context 'with valid attributes' do
-  #     login_user(FactoryGirl.create(:login_user), :context, :authorized => true)
-  #     # before do 
-  #     #   @request.env["devise.mapping"] = Devise.mappings[:user]
-  #     #   user = FactoryGirl.create(:login_user)
-  #     #   allow(request.env['warden']).to receive(:authenticate!).and_return(user)
-  #     #   allow(controller).to receive(:current_user).and_return(user)
-  #     # end
-
-  #     it 'located the requested @user', :authorized => true do
-  #         # login_user(FactoryGirl.create(:login_user))
-  #       user = User.last
-
-  #       put :update, {id: user, user: user.attributes.merge(:current_password => 'password') }
-  #       # binding.pry
-  #       # expect(response.status).to be(302)
-  #       expect(response).to redirect_to(root_path)
-  #       # expect(assigns(:user)).to eq(@user)      
-  #     end
-  #   end
-  # end
+        put :update, {id: user, user: user.attributes.merge(:current_password => 'password') }
+        expect(response.status).to be(302)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+  end
 
 end
