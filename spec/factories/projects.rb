dd2d79183cab4f9d1e549acbd2aeb9773b8f26a1
Carlos Garcia <carlos@par9golf.com>
2014-09-24 01:12:20 -0600

    Updated factories to pass specs

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -10,12 +10,14 @@ FactoryGirl.define do
       name 'Second Project Name'
       description 'MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo'
       due_date_at (Time.now + 1.month)
+      association :creator, factory: :login_user
     end
 
     factory :third_project do
       name 'Third Project Name'
       description 'MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo MyText PartTwo'
       due_date_at (Time.now + 1.month)
+      association :creator, factory: :login_user
     end
   end
 end
