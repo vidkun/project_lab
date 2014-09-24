79c013e860f7bcad149b6c90322b9b944685b670
Carlos Garcia <carlos@par9golf.com>
2014-09-24 13:42:31 -0600

    Update task model to use User as creator

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
   validates :name, presence: true, uniqueness: true
   validates :description, length: { minimum: 50 }
   validates :delivery_minutes, numericality: { greater_than: 0 }
