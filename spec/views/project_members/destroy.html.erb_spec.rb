62728ed6824909f47852ed6762c735402611433d
djmrr <mikedjrussell@gmail.com>
2014-09-25 04:40:39 -0600

    Got tests written for everything EXCEPT: -project_member_spec.rb -project_members_spec.rb
    ***********************************************
    ORIGINAL ISH that needed some help
    **********************************************
    Pending:
      ProjectMembersHelper add some examples to (or delete) /Users/carlos/dev/devpointlabs/work/project_lab/spec/helpers/project_members_helper_spec.rb
        # Not yet implemented
        # ./spec/helpers/project_members_helper_spec.rb:14
      ProjectMember add some examples to (or delete) /Users/carlos/dev/devpointlabs/work/project_lab/spec/models/project_member_spec.rb
        # Not yet implemented
        # ./spec/models/project_member_spec.rb:4
      project_members/create.html.erb add some examples to (or delete) /Users/carlos/dev/devpointlabs/work/project_lab/spec/views/project_members/create.html.erb_spec.rb
        # Not yet implemented
        # ./spec/views/project_members/create.html.erb_spec.rb:4
      project_members/destroy.html.erb add some examples to (or delete) /Users/carlos/dev/devpointlabs/work/project_lab/spec/views/project_members/destroy.html.erb_spec.rb
        # Not yet implemented
        # ./spec/views/project_members/destroy.html.erb_spec.rb:4
      project_members/new.html.erb add some examples to (or delete) /Users/carlos/dev/devpointlabs/work/project_lab/spec/views/project_members/new.html.erb_spec.rb
        # Not yet implemented
        # ./spec/views/project_members/new.html.erb_spec.rb:4
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    ___________FML it's almost 5am______________________
    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    -----------------------------------------------------------
    ------- END TRANSMISSION ----------------------------------
    -----------------------------------------------------------

--- spec/views/project_members/destroy.html.erb_spec.rb
+++ spec/views/project_members/destroy.html.erb_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "project_members/destroy.html.erb", :type => :view do
-
+#it doesn't fail... ...but...still all 
+#these tests are garbage
+#oscar the grouch status? 
+#I LOVE TRASH 
 	describe 'has no view' do 
 		it 'sees no view' do
 			within('html') do
