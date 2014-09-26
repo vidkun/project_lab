7fa12f6af46ddc923377c05af140dd46c979fe63
Carlos Garcia <carlos@par9golf.com>
2014-09-26 01:11:06 -0600

    Added a feature spec

---
+++ spec/support/feature_support.rb
@@ -0,0 +1,8 @@
+module FeatureSupport
+  def log_in_with(email, password)
+    visit new_user_session_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Log in'
+  end
+end
