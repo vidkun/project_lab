c4b4363b664e0732d7683165cefc029e20699cd2
Carlos Garcia <carlos@par9golf.com>
2014-09-26 00:35:34 -0600

    Add scope to remove duplicates in user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :project_members
-  has_many :projects, through: :project_members
+  has_many :projects, -> {distinct}, through: :project_members
   has_many :tasks
 
   devise :database_authenticatable, :registerable,
