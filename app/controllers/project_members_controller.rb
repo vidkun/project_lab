edc001956c688867d14d127ea2a16a5aaab5ebc0
Rocio <rsoto1@hotmail.com>
2014-09-29 11:11:30 -0600

    changes

--- app/controllers/project_members_controller.rb
+++ app/controllers/project_members_controller.rb
@@ -30,3 +30,38 @@ class ProjectMembersController < ApplicationController
     @project = Project.find(params[:project_id])
   end
 end
+
+class ProjectMembersController < ApplicationController
+  before_action :set_project
+
+
+  def new
+    redirect_to project_path(@project), notice: "Could not add member" unless @project && current_user.can_add_member?(@project)
+    @project_member = ProjectMember.new
+  end
+
+  def create
+    @project_member = @project.project_members.build(project_member_params)
+
+    if @project_member.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @project_member = @project.project_members.find_by(id: params[:id]) if @project 
+    notice = current_user.delete_member(@project, @project_member) ? 'Member was successfully deleted' : 'Could not delete member'
+    redirect_to project_path(@project), notice: notice
+  end
+
+  private
+  def project_member_params
+    params.require(:project_member).permit(:project_id, :user_id)
+  end
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+end
