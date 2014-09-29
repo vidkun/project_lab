eb0025e7838d949b21fcb1e3b053a183d7edac07
djmrr <mikedjrussell@gmail.com>
2014-09-29 16:38:30 -0600

    JQuery'd it all! Yeeeee

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,48 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+$( document ).ready(function(){
+	clock();
+	setInterval(clock, 1000);
+	console.log('ready for dat time'); 
+
+	function clock(){
+		var time = new Date()
+		var hr = time.getHours()
+		var min = time.getMinutes()
+		var sec = time.getSeconds()
+		var ampm = " PM "
+		if (hr < 12){
+		ampm = " AM "
+		}
+		if (hr > 12){
+		hr -= 12
+		}
+		if (hr < 10){
+		hr = " " + hr
+		}
+		if (min < 10){
+		min = "0" + min
+		}
+		if (sec < 10){
+		sec = "0" + sec
+		}
+		document.clockForm.clockButton.value = hr + ":" + min + ":" + sec + ampm;		
+		
+	};
+
+		function showDate(){
+		// console.log("Hello World")
+			var date = new Date();
+			var year = date.getYear();
+			if(year < 1000){
+			year += 1900;
+			};
+			var monthArray = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+			alert( monthArray[date.getMonth()] + " " + date.getDate() + ", " + year);
+		}
+	$('#tClkBtn').click(function(){
+		showDate();
+	});
+});
