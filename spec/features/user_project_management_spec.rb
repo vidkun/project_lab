795f33d5352ab609dc257d1e48abf88489394210
Paul Cox <paulcox24@gmail.com>
2014-09-28 21:21:26 -0600

    Feature spec for users seeing others tasks, and status

--- spec/features/user_project_management_spec.rb
+++ spec/features/user_project_management_spec.rb
@@ -1,15 +1,29 @@
 require "rails_helper"
 
 feature "User project management" do
+  background do 
+    @user = User.find_by(name: 'Sam')
+    @user1 = User.find_by(name: 'Fred')
+    log_in_with(@user.email, 'password')
+  end
+    
   scenario "As a user, I can login to the Project Manager and see the projects I have access to" do
-    user = User.find_by(name: 'Sam')
-
-    log_in_with(user.email, 'password')
-
+    visit root_path
     # Expect user to be logged in and see their email at the top of the page
-    expect(page.html).to have_link(user.email, user_path(user))
+    expect(page.html).to have_link(@user.email, user_path(@user))
 
     expect(page).to have_content("Sign Out")
     expect(page).to have_content("This is the project one.")
   end
+
+  scenario "As a user, I can see other peoples’ tasks and their current status" do
+    visit "/users/#{@user1.id}"
+    
+    expect(page).to have_content('Fred')
+    if @user1.tasks.any?
+      expect(page).to have_content('Incomplete' || 'Complete')
+      expect(page).to have_content("#{@user1.tasks.first.name}")
+    end
+  end
+
 end
