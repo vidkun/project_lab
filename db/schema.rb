23b40eece01022b5380c55112e0d040cf0911623
Carlos Garcia <carlos@par9golf.com>
2014-10-02 00:20:22 -0600

    Add new columns for storing github api tokens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926200302) do
+ActiveRecord::Schema.define(version: 20141002055551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20140926200302) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_admin",                         default: false
+    t.string   "github_state"
+    t.string   "github_access_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
