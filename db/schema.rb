8418926ddf509a83dc0ddac06cfe46c0dc62bade
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-03 11:10:29 -0600

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003164733) do
+ActiveRecord::Schema.define(version: 20141003170914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,12 +51,12 @@ ActiveRecord::Schema.define(version: 20141003164733) do
   add_index "tasks", ["user_id"], name: "index_tasks_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                            default: "",    null: false
-    t.string   "encrypted_password",               default: "",    null: false
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                    default: 0,     null: false
+    t.integer  "sign_in_count",                    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 20141003164733) do
     t.integer  "phone",                  limit: 8
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "is_admin",                         default: false
     t.string   "github_state"
     t.string   "github_access_token"
     t.string   "github_username"
