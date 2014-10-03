f7a81c1017138213b0fa9d64b6a975ae6ecb4e6b
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-03 11:07:37 -0600

    update can_admin concern with enum check

--- app/controllers/concerns/can_admin.rb
+++ app/controllers/concerns/can_admin.rb
@@ -2,7 +2,8 @@ module CanAdmin
   extend ActiveSupport::Concern
 
   private
-    def can_admin?
-      redirect_to root_path unless current_user.is_admin?
-    end 
-end   
+
+  def can_admin?
+    redirect_to root_path unless current_user.admin?
+  end
+end
