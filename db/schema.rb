42522c69ff23e045681d07f9b05f16b6f810f019
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-03 10:50:31 -0600

    rake db:migrate to catch up database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141003053121) do
+ActiveRecord::Schema.define(version: 20141003164733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20141003053121) do
     t.string   "github_username"
     t.string   "provider"
     t.string   "uid"
+    t.integer  "role",                             default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
