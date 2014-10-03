ec8cb5f45737b2954f616a2eac5ef8656cee3d22
Carlos Garcia <carlos@par9golf.com>
2014-10-03 02:09:08 -0600

    Added method to populate user data from session

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,6 +27,18 @@ class User < ActiveRecord::Base
     end
   end
 
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+        user.name = data['name']
+        user.github_username = data['login']
+        user.github_access_token = session["devise.github_data"]['credentials']['token']
+        user.github_state = 'completed'
+      end
+    end
+  end
+
   def delete_task(task)
     task_to_delete = self.tasks.find_by(id: task.id)
     task_to_delete ||= task if task.creator == self
