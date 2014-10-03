b34059643340403a22bd2dc65a4eb053a96a6525
Carlos Garcia <carlos@par9golf.com>
2014-10-02 23:32:07 -0600

    Add provided and uid to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141002074601) do
+ActiveRecord::Schema.define(version: 20141003053121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20141002074601) do
     t.string   "github_state"
     t.string   "github_access_token"
     t.string   "github_username"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
