8b6de8c542484d6dbb59b5dba8167bf0f98da6c7
Carlos Garcia <carlos@par9golf.com>
2014-10-06 19:45:09 -0600

    Added admin controller for stats

---
+++ app/controllers/admin/stats_controller.rb
@@ -0,0 +1,5 @@
+class Admin::StatsController < ApplicationController
+  def index
+    # @data = LowLevel::Stats.get_has_many_relationships
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'stats', to: 'stats#index', as: 'stats'
+  end
+
   get 'project_members/new'
 
   get 'project_members/create'
