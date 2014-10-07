6ef049fe6d1d93ed71b1d23745635278cd395793
Carlos Garcia <carlos@par9golf.com>
2014-10-06 19:43:57 -0600

    Example output and smaller code

--- lib/low_level/stats.rb
+++ lib/low_level/stats.rb
@@ -8,11 +8,12 @@ module LowLevel
     HAS_MANY_SEARCH = "ack \" has_many \" -c | awk -F \":\" '{print $2,$1}' | grep -v \"0\" #{PIPE_MINUS_COVERAGE} #{PIPE_MINUS_CURRENT_FILE} | sort -rn"
 
     def self.get_raw(command)
+      # ["3 app/models/user.rb", "3 app/models/project.rb"]
       `#{command}`
     end
 
     def self.get_has_many_relationships
-      results = get_raw(HAS_MANY_SEARCH).split("\n")
+      get_raw(HAS_MANY_SEARCH).split("\n")
     end
   end
 end
