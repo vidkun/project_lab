d61e10acc1f711e87547b58d208bb37f98118e4c
Carlos Garcia <carlos@par9golf.com>
2014-10-20 16:51:28 -0600

    Added admin action to expire cache

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -7,4 +7,9 @@ class Admin::ProjectsController < ApplicationController
     @projects = Project.all
     render 'projects/index'
   end
+
+  def expire_cache
+    expire_fragment(controller: params[:controller], action: 'index')
+    redirect_to action: :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,8 @@ Rails.application.routes.draw do
     resources :projects, only: [:index]
   end
 
+  get 'admin/projects/expire_cache' => 'admin/projects#expire_cache'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
